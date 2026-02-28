c5471881f7e58c9930eb0e12eedd9e538821c1cc
marco <marco@DESKTOP-GCUIOA1>
2026-02-28 22:47:18 +0200

    Q 01

--- Assignment OOP 02/Program.cs
+++ Assignment OOP 02/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Assignment_OOP_02
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_OOP_02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            /*
+
+            a)
+                Public fields allow direct access to class data.
+
+                No control or validation over modifying the balance.
+
+                The internal state of the object can be changed from outside the class.
+
+
+
+            b)
+
+             Change public fields to private.
+
+            Use public properties to access data.
+ 
+            Apply validation inside properties or methods.
+
+
+            c) 
+
+            Breaks encapsulation.
+
+            Allows uncontrolled access to data.
+
+            Makes the class harder to maintain and extend.
+
+            */
+            #endregion
+
+
         }
-    }
+}
 }
