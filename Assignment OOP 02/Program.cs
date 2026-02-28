7da662f24a88c832f8cb570e24c2fca8daafd78b
marco <marco@DESKTOP-GCUIOA1>
2026-02-28 22:24:17 +0200

    Add project files.

---
+++ Assignment OOP 02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_OOP_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
